3b158cd3943eb687c670b99468b3983f9a200679
Mohammad Alsallal <malslal787@gmail.com>
2024-08-12 14:48:43 +0300

    Hero section is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./Components/Header";
+import Hero from "./Components/Hero";
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
       <ButtonGradient />
     </>
--- src/Components/Section.jsx
+++ src/Components/Section.jsx
@@ -4,20 +4,20 @@ const Section = ({
   id,
   crosses,
   crossesOffset,
-  customPadding,
+  customPaddings,
   children,
 }) => {
   return (
     <div
       id={id}
       className={`relative ${
-        customPadding ||
+        customPaddings ||
         `py-10 lg:py-16 xl:py-20 ${crosses ? `lg:py-32 xl:py-40` : ``} ${
           className || ""
         }`
       } `}
     >
-      ${children}
+      {children}
       <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:left-7.5 xl:left-10"></div>
       <div className="hidden absolute top-0 right-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:right-7.5 xl:right-10"></div>
       {crosses && (
