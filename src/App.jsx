58f17fab32831c249d437dd749446725b316b828
Mohammad Alsallal <malslal787@gmail.com>
2024-08-12 20:12:49 +0300

    Roadmap Section is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./Components/Benefits";
 import Collaboration from "./Components/Collaboration";
+import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
 import Pricing from "./Components/Pricing";
+import Roadmap from "./Components/Roadmap";
 import Services from "./Components/Services";
 const App = () => {
   return (
@@ -15,6 +17,8 @@ const App = () => {
         <Collaboration />
         <Services />
         <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
