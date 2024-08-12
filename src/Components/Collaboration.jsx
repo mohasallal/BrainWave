245414efee03dec6d7701394ad4e40c1a1aee5f6
Mohammad Alsallal <malslal787@gmail.com>
2024-08-12 18:53:35 +0300

    Services Section is Done

--- src/Components/Collaboration.jsx
+++ src/Components/Collaboration.jsx
@@ -7,7 +7,7 @@ import { LeftCurve, RightCurve } from "./design/Collaboration";
 const Collaboration = () => {
   return (
     <Section crosses>
-      <div className="container lg:flex">
+      <div className="container lg:flex mb-[10rem]">
         <div className="max-w-[25rem]">
           <h2 className="h2 mb-4 md:mb-8">
             AI Chat App for seamless collaboration
